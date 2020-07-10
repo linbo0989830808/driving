aba167d7102643e139dff22ef11fda406f1caaff
BOYOU LIN <boyoulin@BOYOUde-MacBook-Air.local>
2020-07-11 00:28:52 +0800

    add Country

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = input('請問你出生於哪個國家？ ')
+age = input('請輸入年齡： ')
+age = int(age)
+if country == '台灣' or 'Taiwan': 
+    if age >= 18:
+        print('你可以考駕照')
+    else:
+    	print('你還不能考駕照')
